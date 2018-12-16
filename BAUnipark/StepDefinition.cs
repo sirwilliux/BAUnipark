2a94f659fe6d7cd9c19f7167df27fbfa94a887f7
vj <vj@frontit.dk>
2018-12-16 14:23:17 +0200

    Agree to cookies instead of ignore

--- BAUnipark/StepDefinition.cs
+++ BAUnipark/StepDefinition.cs
@@ -133,7 +133,7 @@ namespace BAUnipark
         public void ThenFillAllThePersonalDataIncludingAllTheAgreementsAndOptionsAvailable()
         {
             Thread.Sleep(1000);
-            Driver.FindElement(By.XPath("//div[@class='closeCookieConsent']")).Click();
+            Driver.FindElement(By.XPath("//a[@class='cookieConsentOK']")).Click();
             Driver.FindElement(By.XPath("//div[@id='step_2']//input[@name='firstname' and @type='text']")).SendKeys("Philoso");
             Driver.FindElement(By.XPath("//div[@id='step_2']//input[@name='lastname' and @type='text']")).SendKeys("Raptor");
             Driver.FindElement(By.XPath("//div[@id='step_2']//input[@name='phone_number' and @type='text']")).SendKeys("+37060000000");
